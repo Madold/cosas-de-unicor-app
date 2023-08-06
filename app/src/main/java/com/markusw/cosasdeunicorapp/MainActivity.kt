a3afd3d14017b4c717eecf40c7468eebd78b03fd
Marco Portacio <marcoportacio4@gmail.com>
2023-08-06 14:55:29 -0500

    Finished dagger hilt setup

--- app/src/main/java/com/markusw/cosasdeunicorapp/MainActivity.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.markusw.cosasdeunicorapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.markusw.cosasdeunicorapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
