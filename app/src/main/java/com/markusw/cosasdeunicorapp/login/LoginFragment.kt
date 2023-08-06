9d35094135c49e38dd524a7834cac1ddc6c87afe
Marco Portacio <marcoportacio4@gmail.com>
2023-08-06 15:27:19 -0500

    Changed java version to 17 and finished LoginScreen initial setup

---
+++ app/src/main/java/com/markusw/cosasdeunicorapp/login/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.markusw.cosasdeunicorapp.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.markusw.cosasdeunicorapp.R
+import com.markusw.cosasdeunicorapp.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
