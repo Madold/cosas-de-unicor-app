2d5e8c8b38f264e95f0789028e036cfacce7f455
Marco Portacio <marcoportacio4@gmail.com>
2023-08-07 20:22:39 -0500

    Finished register screen

--- app/src/main/java/com/markusw/cosasdeunicorapp/register/RegisterFragment.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/register/RegisterFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.markusw.cosasdeunicorapp.R
 import com.markusw.cosasdeunicorapp.databinding.FragmentRegisterBinding
 
@@ -12,6 +13,7 @@ class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+    private val navController by lazy { findNavController() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +25,11 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupEvents()
+    }
+
+    private fun setupEvents() {
+        binding.topAppBar.setNavigationOnClickListener { navController.popBackStack() }
     }
 
     override fun onDestroy() {
