060f7d24bea21e875df0cb203c353a696a9102a6
Marco Portacio <marcoportacio4@gmail.com>
2023-08-07 21:51:34 -0500

    Created LoginViewModel

---
+++ app/src/main/java/com/markusw/cosasdeunicorapp/login/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.markusw.cosasdeunicorapp.login
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class LoginViewModel @Inject constructor(
+
+): ViewModel() {
+
+
+
+}
