a8d23bddd77b5bd76dcf0805922bd5da67f1d1a5
Marco Portacio <marcoportacio4@gmail.com>
2023-08-10 21:04:25 -0500

    Finished configuration of timber and logger libraries

--- app/src/main/java/com/markusw/cosasdeunicorapp/App.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/App.kt
@@ -4,18 +4,34 @@ import android.app.Application
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.BuildConfig
 import com.google.firebase.ktx.Firebase
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if (BuildConfig.DEBUG) {
-            Firebase.analytics.setAnalyticsCollectionEnabled(false)
-            Timber.plant(Timber.DebugTree())
-        }
+
+        Firebase.analytics.setAnalyticsCollectionEnabled(false)
+        val formatStrategy = PrettyFormatStrategy
+            .newBuilder()
+            .showThreadInfo(true)
+            .methodOffset(5)
+            .tag("")
+            .build()
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority, "-$tag", message, t)
+            }
+        })
+
+        Timber.d("Starting App...")
+
 
     }
 }
