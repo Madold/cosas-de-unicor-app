da9be06de26c1c81bb58f63a09acaf0514d073d3
Marco Portacio <marcoportacio4@gmail.com>
2023-08-10 20:34:06 -0500

    Added firebase storage to FirebaseModule to allow it to be injected

--- app/src/main/java/com/markusw/cosasdeunicorapp/di/FirebaseModule.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/di/FirebaseModule.kt
@@ -3,6 +3,8 @@ package com.markusw.cosasdeunicorapp.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,5 +18,9 @@ object FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = Firebase.auth
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage(): FirebaseStorage = Firebase.storage
 
 }
--- app/src/main/java/com/markusw/cosasdeunicorapp/home/HomeViewModel.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/HomeViewModel.kt
@@ -2,10 +2,8 @@ package com.markusw.cosasdeunicorapp.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.markusw.cosasdeunicorapp.core.DispatcherProvider
+import com.markusw.cosasdeunicorapp.core.utils.Resource
 import com.markusw.cosasdeunicorapp.domain.services.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,7 +16,14 @@ class HomeViewModel @Inject constructor(
 ) : ViewModel() {
     fun onCloseSession() {
         viewModelScope.launch(dispatchers.io) {
-            authService.logout()
+            when (val authResult =  authService.logout()) {
+                is Resource.Error -> {
+
+                }
+                is Resource.Success -> {
+
+                }
+            }
         }
     }
 
