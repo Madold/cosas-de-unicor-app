170b59976e5a0443c8239275427a91bc22e9030a
Marco Portacio <marcoportacio4@gmail.com>
2023-08-11 21:11:31 -0500

    Optimized imports

--- app/src/main/java/com/markusw/cosasdeunicorapp/MainActivity.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.markusw.cosasdeunicorapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
