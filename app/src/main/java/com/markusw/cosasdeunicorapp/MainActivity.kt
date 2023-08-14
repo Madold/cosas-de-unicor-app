a1d1a851471a8c7ad4054d8104ebf4dfcdcfb6dd
Marco Portacio <marcoportacio4@gmail.com>
2023-08-13 20:05:05 -0500

    Finished splash screen implementation

--- app/src/main/java/com/markusw/cosasdeunicorapp/MainActivity.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.markusw.cosasdeunicorapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.markusw.cosasdeunicorapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
