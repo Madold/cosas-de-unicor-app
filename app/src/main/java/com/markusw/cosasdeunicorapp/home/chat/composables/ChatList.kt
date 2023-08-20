3b4a526ac6982ec2d080858d8387623ca2c6570a
Marco Portacio <marcoportacio4@gmail.com>
2023-08-19 22:14:04 -0500

    When a new message is sent or received the list automatically scrolls down

--- app/src/main/java/com/markusw/cosasdeunicorapp/home/chat/composables/ChatList.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/chat/composables/ChatList.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.markusw.cosasdeunicorapp.data.model.Message
@@ -14,10 +16,18 @@ fun ChatList(
     items: List<Message>,
     modifier: Modifier = Modifier
 ) {
+
+    val scrollState = rememberLazyListState()
+
+    LaunchedEffect(key1 =  items) {
+        scrollState.animateScrollToItem(items.lastIndex)
+    }
+
     LazyColumn(
         modifier = modifier
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        state = scrollState
     ) {
         items(items) { message ->
             ChatItem(
