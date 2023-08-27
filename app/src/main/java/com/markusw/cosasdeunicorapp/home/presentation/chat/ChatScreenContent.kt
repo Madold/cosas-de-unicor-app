8602183220e4002cbdf5c9ab1f1d95efacfc5897
Marco Portacio <marcoportacio4@gmail.com>
2023-08-26 22:36:33 -0500

    Added a top app bar to chat screen

--- app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/chat/ChatScreenContent.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/chat/ChatScreenContent.kt
@@ -14,6 +14,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,6 +27,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.markusw.cosasdeunicorapp.R
 import com.markusw.cosasdeunicorapp.core.ext.isScrolledToTheEnd
@@ -80,6 +83,13 @@ fun ChatScreenContent(
                     }
                 )
             }
+        },
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.global_chat))
+                }
+            )
         },
         floatingActionButton = {
             FloatingActionButton(
