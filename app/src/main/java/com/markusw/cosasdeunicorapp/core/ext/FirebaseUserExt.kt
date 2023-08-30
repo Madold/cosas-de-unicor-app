236821890760246f511bb5ab043e52f1043d0394
Marco Portacio <marcoportacio4@gmail.com>
2023-08-29 20:17:29 -0500

    Fixed linter errors

--- app/src/main/java/com/markusw/cosasdeunicorapp/core/ext/FirebaseUserExt.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/core/ext/FirebaseUserExt.kt
@@ -5,8 +5,8 @@ import com.markusw.cosasdeunicorapp.home.domain.model.User
 
 fun FirebaseUser.toUserModel(): User {
     return User(
-        this.displayName,
-        this.email,
+        this.displayName!!,
+        this.email!!,
         this.photoUrl.toString(),
         this.uid
     )
