20012f70fc50751f70c98c4810feecb1c25fff94
Marco Portacio <marcoportacio4@gmail.com>
2023-08-31 10:34:20 -0500

    Added top app bar to chat screen

--- app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/chat/ChatScreenContent.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/chat/ChatScreenContent.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -25,6 +27,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.markusw.cosasdeunicorapp.R
 import com.markusw.cosasdeunicorapp.core.ext.isScrolledToTheEnd
@@ -67,6 +70,11 @@ fun ChatScreenContent(
     Scaffold(
         modifier = Modifier
             .fillMaxSize(),
+        topBar = {
+                 CenterAlignedTopAppBar(title = { 
+                     Text(text = stringResource(id = R.string.global_chat))
+                 })
+        },
         bottomBar = {
             Box(
                 modifier = Modifier.fillMaxWidth(),
--- app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/chat/composables/ChatList.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/chat/composables/ChatList.kt
@@ -1,17 +1,15 @@
 package com.markusw.cosasdeunicorapp.home.presentation.chat.composables
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.markusw.cosasdeunicorapp.core.ext.isSecondLastItemVisible
@@ -49,12 +47,7 @@ fun ChatList(
         }
         item {
             if (isFetchingPreviousMessages) {
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
+                LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
             }
         }
     }
