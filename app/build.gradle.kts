e74a9c5d0a1fc5189a84a3f39c31640751acd81e
Marco Portacio <marcoportacio4@gmail.com>
2023-09-02 22:55:25 -0500

    Downgraded compose bom version due to incompatibilities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
