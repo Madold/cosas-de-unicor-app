2b8e1b37c69ef53aae76a144a7a3a6d42a58901e
Marco Portacio <marcoportacio4@gmail.com>
2024-01-12 11:53:32 -0500

    Improved News Card UI design

--- app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/news/composables/NewsCard.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/news/composables/NewsCard.kt
@@ -7,17 +7,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
+import com.markusw.cosasdeunicorapp.R
 import com.markusw.cosasdeunicorapp.core.presentation.ExpandableText
 import com.markusw.cosasdeunicorapp.home.domain.model.News
 
@@ -30,50 +33,49 @@ fun NewsCard(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .clickable { onNewsClicked(news) }
             .padding(8.dp),
         horizontalArrangement = Arrangement.Center
     ) {
-        Card(
-            modifier = modifier
-                .fillMaxWidth(0.9f),
-            shape = RoundedCornerShape(16.dp)
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
         ) {
-            Column(
+            SubcomposeAsyncImage(
+                model = news.coverUrl,
+                contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clickable { onNewsClicked(news) }
-                    .padding(8.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.FillWidth
+            )
+            Text(
+                text = news.title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            /*ExpandableText(
+                text = news.content,
+                collapsedMaxLine = 5,
+                showMoreText = "...Mostrar más",
+                showLessText = " Mostrar menos",
+                textAlign = TextAlign.Justify,
+            )*/
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
             ) {
-                SubcomposeAsyncImage(
-                    model = news.coverUrl,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(8.dp)),
-                    contentScale = ContentScale.FillWidth
-                )
-                Text(
-                    text = news.title,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Divider()
-                /*Text(
-                    text = news.content,
-                    maxLines = 10,
-                    style = MaterialTheme.typography.bodyLarge,
-                    overflow = TextOverflow.Ellipsis,
-                    textAlign = TextAlign.Justify
-                )*/
-                ExpandableText(
-                    text = news.content,
-                    collapsedMaxLine = 5,
-                    showMoreText = "...Mostrar más",
-                    showLessText = " Mostrar menos",
-                    textAlign = TextAlign.Justify,
-                )
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_heart),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
             }
         }
     }
+    Divider()
 }
 
 @Composable
