281c634ee763866291734e55e8ee8501488767f3
Marco Portacio <marcoportacio4@gmail.com>
2024-01-26 21:57:16 -0500

    Fixed crashlytics instalation

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.16" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
 
 buildscript {
