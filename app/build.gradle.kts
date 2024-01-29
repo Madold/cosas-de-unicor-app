53cf99ac874e184628da240fe989d9e07c8340e5
Marco Portacio <marcoportacio4@gmail.com>
2024-01-29 12:10:48 -0500

    renamed version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.markusw.cosasdeunicorapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "beta-1-mary"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
