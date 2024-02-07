27927c07b5ff5db2121786f5b4eee95264e66499
Marco Portacio <marcoportacio4@gmail.com>
2024-02-07 14:11:51 -0500

    Imports cleanup

--- app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/HomeFragment.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.animation.shrinkOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
