108bbd6aa3438ab7ba914abcc4984375cc16a47d
Marco Portacio <marcoportacio4@gmail.com>
2024-02-12 20:31:09 -0500

    Added press animation to feature card

--- app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/more/composables/FeatureCard.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/home/presentation/more/composables/FeatureCard.kt
@@ -1,6 +1,9 @@
 package com.markusw.cosasdeunicorapp.home.presentation.more.composables
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,8 +12,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -20,13 +26,24 @@ fun FeatureCard(
     icon: @Composable () -> Unit,
     onClick: () -> Unit
 ) {
+
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val scale by animateFloatAsState(targetValue = if (isPressed) 0.9f else 1f, label = "ScaleAnimation")
+
     Card(
-        elevation = CardDefaults.cardElevation(16.dp)
+        elevation = CardDefaults.cardElevation(16.dp),
+        modifier = Modifier
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = onClick
+            )
+            .scale(scale)
     ) {
         Column(
             modifier = modifier
                 .fillMaxWidth()
-                .clickable(onClick = onClick)
                 .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
