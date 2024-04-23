781a789a6e06db4e5e2e802b7da17266a455a313
Marco Portacio <marcoportacio4@gmail.com>
2024-04-22 19:25:49 -0500

    Cleaned imports

--- app/src/main/java/com/markusw/cosasdeunicorapp/teacher_rating/presentation/TeacherDetailsScreen.kt
+++ app/src/main/java/com/markusw/cosasdeunicorapp/teacher_rating/presentation/TeacherDetailsScreen.kt
@@ -10,10 +10,8 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
